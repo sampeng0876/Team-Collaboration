90232be9c8471a62a7b4987883e97530fa9cc7ce
Sam Peng <bob0876@outlook.com>
2023-05-08 17:10:15 -0700

    5/8 updates

--- date_picker.py
+++ date_picker.py
@@ -3,13 +3,16 @@ from tkcalendar import DateEntry
 
 # Define the options for the dropdown menu
 #date_list = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
-time_slots_list = ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23']
+time_slots_list_1 = ['00:00','01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00']
+time_slots_list_2 = ['00:00','01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00','09:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00','22:00','23:00']
+available_time_slots = ['02','03','04','05','06','07','08','10']
 
 # Create a function to get the selected value and close the window
 def on_select():
-    global date_picker, time_picker
+    global date_picker, time_picker_1,time_picker_2
     date_picker = cal.get_date().strftime('%Y-%m-%d')
-    time_picker = var.get()
+    time_picker_1 = var1.get()
+    time_picker_2 = var2.get()
     
     root.destroy()
 
@@ -17,43 +20,46 @@ def on_select():
 root = tk.Tk()
 
 # Create a variable to store the selected option
-var = tk.StringVar(root)
-
+var1 = tk.StringVar(root)
+var2 = tk.StringVar(root)
 # Set the default value of the dropdown
-var.set(time_slots_list[0])
+var1.set(time_slots_list_1[0])
+var2.set(time_slots_list_2[0])
 
 # Create the dropdown and add the options
-dropdown = tk.OptionMenu(root, var, *time_slots_list)
-dropdown.pack()
+dropdown1 = tk.OptionMenu(root, var1, *time_slots_list_1)
+dropdown1.pack(padx=10, pady=10)
+dropdown2 = tk.OptionMenu(root, var2, *time_slots_list_2)
+dropdown2.pack(padx=10, pady=10)
 
 # Create a calendar widget
 cal = DateEntry(root)
-cal.pack()
+cal.pack(padx=10, pady=10)
 
 # Create a button to submit the selection and close the window
 submit_button = tk.Button(root, text="Submit", command=on_select)
-submit_button.pack()
+submit_button.pack(padx=10, pady=10)
 
 # Start the mainloop to display the window
 root.mainloop()
 
-
-available_time_slots = ['02','03','04','05','06','07','08']
-
 print(type(date_picker))
-print("Selected " + date_picker[-2:])
-print("Selected " + time_picker + ":00")
+print("Selected " + date_picker)
+print("Selected " + time_picker_1 + " to "+ time_picker_2)
 appt_date = '9'
 print(type(appt_date))
+print("Available Times: " + str(available_time_slots))
 for i in available_time_slots:
     # print([i], end=' ')
     # print(i, end=' ')
     # print(time_picker)   
-    if i == time_picker:
-        #print(i)
+    if i in range(0,23):
+        print(i)
+        print(time_picker_1[:2])
+        print(time_picker_2[:2])
         print("Successfully Booked " + str(date_picker) + ' at ' + i  + ":00")
         break        
-else: print(time_picker + ":00" + " is Not Available")
+else: print("No Available Times from " + time_picker_1 + " to "+ time_picker_2)
     
 
     
--- get_apm_appt.py
+++ get_apm_appt.py
@@ -90,16 +90,18 @@ sleep(1)
 
 #Empty Container#
 driver.find_element(By.XPATH,'//*[@id="containerName"]').send_keys(ctnrno) 
+
 #Submit
 driver.find_element(By.XPATH,'//*[@id="formcontent"]/form/div[2]/div[2]/button').click() 
 sleep(1)
 
 #OWN CHASSIS?
 driver.find_element(By.XPATH,'//*[@id="ipgrid_0_own_chassis?"]/i').click()
+sleep(1)
 
 #NO
 driver.find_element(By.XPATH,'//*[@id="ipgrid_0_own_chassis?"]/div[2]/div[2]/span').click()
-
+sleep(1)
 #
 #driver.find_element(By.XPATH,'//*[@id="ipgrid_0"]/div[3]/div[2]/div/div[1]/div').click()
 
@@ -122,6 +124,7 @@ date_str = date_to_select.strftime("%m/%d/%Y")
 # Click the date picker element to open the date picker
 calendar_picker.click()
 print("Clicked Calendar")
+# sleep(1)
 # Locate the dates of this month
 dates = driver.find_elements(by="xpath", value='//div[@class="react-flex-view align-content-center justify-content-center react-datepicker-picker day current"]')
 
@@ -132,14 +135,15 @@ for date in dates:
     if date_text == appt_date:
         # print("Found it")
         date.click()        
-        print("Clicked "+ date_text)
-        # sleep(5)
+        print("Clicked Date")
+        # sleep(1)
         break
 
 # Click Time Slots
 time_clicker = wait.until(EC.presence_of_element_located((By.XPATH,'//*[@id="ipgrid_0_slot"]/i')))
 time_clicker.click()
-
+# sleep(1)
+print("Clicked Time Slots")
 # Locate the Appointment Time Slots
 time_slots_container = driver.find_elements(by="xpath", value='//*[@class="visible menu transition"]/div') # //*[@id="ipgrid_0_slot"]/div[2]/div[1] SAME AS //div[@class="visible menu transition"]/div
 
@@ -165,9 +169,9 @@ for time_slot in available_time_slots:
     # print(time_picker)   
     if time_slot == time_picker:
         print(time_slot)
-        print("Successfully Booked " + str(date_picker) + ' at ' + time_slot  + ":00")
+        print("Successfully Booked " + str(date_picker) + ' at ' + time_slot)
         break        
-else: print(time_picker + ":00" + " is Not Available")
+else: print(time_picker + " is Not Available")
 
 
 
