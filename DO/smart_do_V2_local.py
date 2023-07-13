b7103e1f39d05dc3a731501a8bdba9d386097299
Sam Peng <bob0876@outlook.com>
2023-07-12 17:43:50 -0700

    7/12 UPDATE

--- DO/smart_do_V2_local.py
+++ DO/smart_do_V2_local.py
@@ -72,7 +72,7 @@ def process_pdf(pdf_paths):
 
 def scan_and_process_files():
     pdf_paths = []
-    directory = r'G:\My Drive\Company\FaCai\DO\SMART'
+    directory = r'K:\My Drive\Company\FaCai\DO\SMART'
     for filename in os.listdir(directory):
         if filename.endswith(".pdf"):
             pdf_paths.append(os.path.join(directory, filename))
